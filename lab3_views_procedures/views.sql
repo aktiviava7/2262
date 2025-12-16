a9611f6af7a4fc3392b24c6b72a942db7ec322e8
aktiviava7 <amerzej@gmail.com>
2025-12-16 17:55:00 +0800

    Create views.sql

---
+++ lab3_views_procedures/views.sql
@@ -0,0 +1,26 @@
+-- Процедура: добавление нового пользователя
+CREATE OR REPLACE PROCEDURE add_user(
+    p_full_name VARCHAR,
+    p_email VARCHAR,
+    p_created_at DATE
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO users (full_name, email, created_at)
+    VALUES (p_full_name, p_email, p_created_at);
+END;
+$$;
+
+-- Процедура: создание заказа
+CREATE OR REPLACE PROCEDURE create_order(
+    p_user_id INT,
+    p_order_date DATE
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO orders (user_id, order_date)
+    VALUES (p_user_id, p_order_date);
+END;
+$$;
